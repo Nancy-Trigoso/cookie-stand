88ee848bb436b9566d99c22dd425048464f3745b
Nancy-Trigoso <nancytrigoso1998@gmail.com>
2024-06-25 23:53:34 -0500

    Instruccion numero 3

--- js/app.js
+++ js/app.js
@@ -25,3 +25,60 @@ const paris = new Location("Paris", 20, 37, 2.3, []);
 const lima = new Location("Lima", 2, 16, 4.6, []);
 
 const hours=['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+const stores=[seattle, tokyo, dubai, paris, lima];
+function random(min, max){
+    return Math.floor(Math.random() * (max - min) + min);
+}
+function estimateSale(store){
+    const sale=[];
+    for(let i=0;i<hours.length;i++){
+        const numCustomers=random(store.minClientPerHour,store.maxClientPerHour);
+        const hoursSale=Math.ceil(numCustomers*store.agvCookiePerSale);
+        sale.push(hoursSale);
+    }
+    return sale;
+}
+
+function render(store){
+    const tr = document.createElement('tr');
+    const tdName = document.createElement('td');
+    tdName.textContent = store.locationName;
+    tr.appendChild(tdName);
+    let total = 0;
+    for(let i = 0;i<hours.length;i++){
+        const td = document.createElement('td');
+        td.textContent = store.cookieEachHour[i];
+        tr.appendChild(td);
+        total += store.cookieEachHour[i];
+    }
+    const td = document.createElement('td');
+    td.textContent = total;
+    tr.appendChild(td);
+    return tr;
+}
+
+function runSales(){
+    const root = document.getElementById('root');
+    const table = document.createElement('table');
+    const trHeader = document.createElement('tr');
+    const thName = document.createElement('th');
+    thName.textContent = 'Tienda';
+    trHeader.appendChild(thName);
+    for(let i =0;i< hours.length;i++){
+        const th = document.createElement('th');
+        th.textContent = hours[i];
+        trHeader.appendChild(th);
+    }
+    const thTotal = document.createElement('th');
+    thTotal.textContent = 'Totals';
+    trHeader.appendChild(thTotal);
+    table.appendChild(trHeader);
+    for(let i =0;i< stores.length;i++){
+        stores[i].estimate();
+        const tr = render(stores[i]);
+        table.appendChild(tr);
+    }
+    root.appendChild(table);
+}
+
+runSales();
