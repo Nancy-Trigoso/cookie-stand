19b17ac983d5f5d5020f9d19bcab17008704e995
Nancy-Trigoso <nancytrigoso1998@gmail.com>
2024-07-19 14:52:26 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -175,11 +175,11 @@ newStoreForm.addEventListener('submit',
     function (event){
         event.preventDefault();
         const locationName = event.target.locationName.value;
-        const minCustomersPerHour = parseInt(event.target.minCustomersPerHour.value);
-        const maxCustomersPerHour = parseInt(event.target.maxCustomersPerHour.value);
-        const avgCookiesPerSale = parseInt(event.target.avgCookiesPerSale.value);
+        const minCustomersPerHour = parseInt(event.target.minClientPerHour.value);
+        const maxCustomersPerHour = parseInt(event.target.maxClientPerHour.value);
+        const avgCookiesPerSale = parseInt(event.target.avgCookiePerSale.value);
 
-        const newLocation = new Location(locationName, '', '', '', [], minClientPerHour,maxClientPerHour , agvCookiePerSale)
+        const newLocation = new Location(locationName, '', '', '', [], minCustomersPerHour, maxCustomersPerHour, avgCookiesPerSale)
         newLocation.estimate(this);
         stores.push(newLocation);
         deleteTable();
